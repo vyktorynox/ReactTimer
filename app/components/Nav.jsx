6962383194ec92d8bc4b411a7323fd1338219748
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2017-08-10 12:48:10 -0400

    Added timer and countdown pages

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,14 +8,10 @@ var Nav = () => {
         <ul className="menu">
           <li className="menu-text">Timer App</li>
           <li>
-            <IndexLink to="/" activeClassName="active" activeStyle={{
-              fontWeight: 'bold'
-            }}>Timer</IndexLink>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
           </li>
           <li>
-            <Link to="/countdown" activeClassName="active" activeStyle={{
-              fontWeight: 'bold'
-            }}>Countdown</Link>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
           </li>
         </ul>
       </div>
