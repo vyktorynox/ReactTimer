5691e88d9b93c1df0069cff4cdaea3e7364e7e40
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2017-08-14 16:52:28 -0400

    Finalize tests and added page title

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -31,5 +31,25 @@ describe('Countdown', () => {
         done();
       },3001)
     });
+    it('should pause countdown on paused status', ()=>{
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(()=>{
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+      },1001);
+    });
+    it('should stop countdown and reset count to zero on stopped status', ()=>{
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(()=>{
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+      },1001);
+    });
   });
 });
